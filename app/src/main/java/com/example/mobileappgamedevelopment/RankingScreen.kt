e7efa8f51e21572fca5fa8eecfa5ad91abcda6c0
mars <shortmariah25@gmail.com>
2025-02-24 01:58:06 +0800

    change font type

--- app/src/main/java/com/example/mobileappgamedevelopment/RankingScreen.kt
+++ app/src/main/java/com/example/mobileappgamedevelopment/RankingScreen.kt
@@ -1,12 +1,15 @@
 package com.example.mobileappgamedevelopment
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -28,8 +31,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil3.compose.AsyncImage
@@ -75,10 +80,19 @@ fun RankingScreen(
             )
         }
     } else {
+        // Background Image
+        Image(
+            painter = painterResource(id = R.drawable.ranking),
+            contentDescription = "Background Image",
+            modifier = Modifier.fillMaxSize(),
+            contentScale = ContentScale.Crop // Adjusts image scaling
+        )
         LazyColumn(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(16.dp)
+                .offset( 0.dp, 200.dp)
+
         ) {
             items(users) { user ->
                 UserRow(user = user)
--- app/src/main/java/com/example/mobileappgamedevelopment/ui/theme/Type.kt
+++ app/src/main/java/com/example/mobileappgamedevelopment/ui/theme/Type.kt
@@ -2,14 +2,19 @@ package com.example.mobileappgamedevelopment.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.mobileappgamedevelopment.R
 
+val coinyFont = FontFamily(
+    Font(R.font.coiny)
+)
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = coinyFont,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
