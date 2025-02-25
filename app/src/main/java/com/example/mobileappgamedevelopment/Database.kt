96e1de825019bfd8c5f1ac9e4da659bce1cc529f
19029489 <2202384@sit.singaporetech.edu.sg>
2025-02-25 15:52:25 +0800

    nothing much

--- app/src/main/java/com/example/mobileappgamedevelopment/Database.kt
+++ app/src/main/java/com/example/mobileappgamedevelopment/Database.kt
@@ -1,20 +1,12 @@
 package com.example.mobileappgamedevelopment
 
-import android.content.Context
-import android.graphics.drawable.BitmapDrawable
 import android.util.Log
 import androidx.core.net.toUri
-import coil3.Bitmap
-import coil3.ImageLoader
-import coil3.request.ImageRequest
-import coil3.request.SuccessResult
-import com.google.firebase.database.ValueEventListener
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.SetOptions
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.FirebaseStorage
-import kotlinx.coroutines.runBlocking
 import java.io.File
 
 class Database {
